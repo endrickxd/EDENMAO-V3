52036d4defe9acf17ef13effe65996977e40d88e
Orlando Abreu <109491615+OrlandoNK@users.noreply.github.com>
2024-10-31 15:36:49 -0400

    cambio de api

--- Edenmao.IU.WebAPI/Program.cs
+++ Edenmao.IU.WebAPI/Program.cs
@@ -1,42 +1,25 @@
-using Microsoft.EntityFrameworkCore;
-using Edenmao.Infrastructure.Data;
-using Edenmao.Infrastructure.FileLogger;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(op =>
-{
-    op.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
-});
 
-builder.Services.AddLogging(loggingBuilder =>
-{
-    loggingBuilder.AddProvider(new FileLoggerProvider("logs.txt"));
-});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
