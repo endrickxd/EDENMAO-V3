39bc59a48cf2043f4173aace6d626cab655ed6d0
Orlando Abreu <joseorlandoabreucorniel@gmail.com>
2024-12-04 19:48:15 -0400

    Update ClienteServices.cs

--- Edenmao.UI.Frontend/Services/ClienteServices.cs
+++ Edenmao.UI.Frontend/Services/ClienteServices.cs
@@ -1,40 +1,46 @@
-﻿using Edenmao.UI.Frontend.Modals;
+﻿using Edenmao.Core.DTOs.Cliente;
+using Edenmao.Core.DTOs.Usuario;
+using Edenmao.Domain.Entities;
 using System.Net.Http.Json;
 
 namespace Edenmao.UI.Frontend.Services
 {
-	//[Route("api/Clientes")]
-	public class ClienteServices
-	{
-		private readonly HttpClient _httpClient;
-		public ClienteServices(HttpClient httpClient)
-		{
-			_httpClient = httpClient;
-		}
-		public async Task<IEnumerable<Clientesp>> GetAllClientes()
-		{
-			return await _httpClient.GetFromJsonAsync<IEnumerable<Clientesp>>("api/Cliente/ObtenerClientes");
-		}
-		public async Task<Clientesp> GetClientesById(int id)
-		{
-			return await _httpClient.GetFromJsonAsync<Clientesp>($"api/Cliente/ObtenerClientePorID/{id}");
-		}
-		public async Task CreateClientes(Clientesp clientes)
-		{
-			await _httpClient.PostAsJsonAsync("api/Cliente/CrearCliente", clientes);
-		}
-		public async Task UpdateClientes(int id, Clientesp clientes)
-		{
-			var response = await _httpClient.PutAsJsonAsync($"api/Cliente/{id}", clientes);
-			if (!response.IsSuccessStatusCode)
-			{
-				var errorMessage = await response.Content.ReadAsStringAsync();
-				throw new Exception($"Error al actualizar las Clientes: {errorMessage}");
-			}
-		}
-		public async Task DeleteClientes(int id)
-		{
-			await _httpClient.DeleteAsync($"api/Cliente/EliminarCliente/{id}");
-		}
-	}
+    //[Route("api/Clientes")]
+    public class ClienteServices
+    {
+        private readonly HttpClient _httpClient;
+        public ClienteServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<ClienteDTO>> GetAllClientes()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<ClienteDTO>>("api/Clientes");
+        }
+        public async Task<ClienteDTO> GetClientesById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<ClienteDTO>($"api/Clientes/{id}");
+        }
+        public async Task<IEnumerable<UsuarioDTO>> GetAllUsuario()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<UsuarioDTO>>("api/Usuarios");
+        }
+        public async Task CreateClientes(ClienteDTO clientes)
+        {
+            await _httpClient.PostAsJsonAsync("api/Clientes", clientes);
+        }
+        public async Task UpdateClientes(int id, ClienteDTO clientes)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Clientes/{id}", clientes);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al actualizar las Clientes: {errorMessage}");
+            }
+        }
+        public async Task DeleteClientes(int id)
+        {
+            await _httpClient.DeleteAsync($"api/Clientes/{id}");
+        }
+    }
 }
