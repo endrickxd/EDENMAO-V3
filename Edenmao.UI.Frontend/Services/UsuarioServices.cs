b77e0d70b273a7f8911e01de1d38a7237967a57d
Orlando Abreu <joseorlandoabreucorniel@gmail.com>
2024-12-04 19:50:50 -0400

    Update UsuarioServices.cs

--- Edenmao.UI.Frontend/Services/UsuarioServices.cs
+++ Edenmao.UI.Frontend/Services/UsuarioServices.cs
@@ -1,40 +1,44 @@
-﻿using Edenmao.UI.Frontend.Modals;
+﻿using Edenmao.Core.DTOs.Rol;
+using Edenmao.Core.DTOs.Usuario;
 using System.Net.Http.Json;
 
 namespace Edenmao.UI.Frontend.Services
 {
-	public class UsuarioServices
-	{
-		private readonly HttpClient _httpClient;
-		public UsuarioServices(HttpClient httpClient)
-		{
-			_httpClient = httpClient;
-		}
-		public async Task<IEnumerable<Usuariop>> GetAllUsuario()
-		{
-			return await _httpClient.GetFromJsonAsync<IEnumerable<Usuariop>>("api/Usuario/ObtenerUsuarios");
-		}
-		public async Task<Usuariop> GetUsuarioById(int id)
-		{
-			return await _httpClient.GetFromJsonAsync<Usuariop>($"api/Usuario/ObtenerUsuarioPorID/{id}");
-		}
-		public async Task CreateUsuarios(Usuariop Usuario)
-		{
-			await _httpClient.PostAsJsonAsync("api/Usuarios/CrearUsuario", Usuario);
-		}
-		
-		public async Task UpdateUsuarios(int id, Usuariop Usuario)
-		{
-			var response = await _httpClient.PutAsJsonAsync($"api/Usuarios/{id}", Usuario);
-			if (!response.IsSuccessStatusCode)
-			{
-				var errorMessage = await response.Content.ReadAsStringAsync();
-				throw new Exception($"Error al actualizar las Usuarios: {errorMessage}");
-			}
-		}
-		public async Task DeleteUsuarios(int id)
-		{
-			await _httpClient.DeleteAsync($"api/Usuarios/EliminarUsuario/{id}");
-		}
-	}
+    public class UsuarioServices
+    {
+        private readonly HttpClient _httpClient;
+        public UsuarioServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<UsuarioDTO>> GetAllUsuario()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<UsuarioDTO>>("api/Usuarios");
+        }
+        public async Task<UsuarioDTO> GetUsuarioById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<UsuarioDTO>($"api/Usuarios/{id}");
+        }
+        public async Task CreateUsuarioes(UsuarioDTO Usuario)
+        {
+            await _httpClient.PostAsJsonAsync("api/Usuarios", Usuario);
+        }
+        public async Task<IEnumerable<RolDTO>> GetAllRol()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<RolDTO>>("api/Roles");
+        }
+        public async Task UpdateCategorias(int id, UsuarioDTO Usuario)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Usuarios/{id}", Usuario);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al actualizar las Usuarios: {errorMessage}");
+            }
+        }
+        public async Task DeleteCategorias(int id)
+        {
+            await _httpClient.DeleteAsync($"api/Usuarios/{id}");
+        }
+    }
 }
