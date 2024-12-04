4b3b081276d735ec3e8c54a2f2274cf60559cd55
Orlando Abreu <joseorlandoabreucorniel@gmail.com>
2024-12-04 19:48:54 -0400

    Update PersonificacionesServices.cs

--- Edenmao.UI.Frontend/Services/PersonificacionesServices.cs
+++ Edenmao.UI.Frontend/Services/PersonificacionesServices.cs
@@ -1,39 +1,41 @@
-﻿using Edenmao.UI.Frontend.Prueba;
+﻿using Edenmao.Core.DTOs.Categoria;
+using Edenmao.Core.DTOs.Personificacion;
 using System.Net.Http.Json;
 
 namespace Edenmao.UI.Frontend.Services
 {
-	public class PersonificacionesServices
-	{
-			private readonly HttpClient _httpClient;
-			public PersonificacionesServices(HttpClient httpClient)
-			{
-				_httpClient = httpClient;
-			}
-			public async Task<IEnumerable<Personificacionp>> GetAllPersonificacion()
-			{
-				return await _httpClient.GetFromJsonAsync<IEnumerable<Personificacionp>>("api/Personificacion/ObtenerPersonificaciones");
-			}
-			public async Task<Personificacionp> GetPersonificacionById(int id)
-			{
-				return await _httpClient.GetFromJsonAsync<Personificacionp>($"api/Personificacion/ObtenerPersonificacionPorID/{id}");
-			}
-			public async Task CreatePersonificaciones(Personificacionp personificacion)
-			{
-				await _httpClient.PostAsJsonAsync("api/Personificacion/CrearPersonificacion", personificacion);
-			}
-			public async Task UpdatePersonificaciones(int id, Personificacionp personificacion)
-			{
-				var response = await _httpClient.PutAsJsonAsync($"api/Personificacion/{id}", personificacion);
-				if (!response.IsSuccessStatusCode)
-				{
-					var errorMessage = await response.Content.ReadAsStringAsync();
-					throw new Exception($"Error al actualizar las Personificacion: {errorMessage}");
-				}
-			}
-			public async Task DeletePersonificaciones(int id)
-			{
-				await _httpClient.DeleteAsync($"api/Personificacion/EliminarPersonificacion/{id}");
-			}
-		}
+    //api/Personificaciones
+    public class PersonificacionServices
+    {
+        private readonly HttpClient _httpClient;
+        public PersonificacionServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<PersonificacionDTO>> GetAllPersonificacion()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<PersonificacionDTO>>("api/Personificaciones");
+        }
+        public async Task<PersonificacionDTO> GetPersonificacionById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<PersonificacionDTO>($"api/Personificaciones/{id}");
+        }
+        public async Task CreatePersonificaciones(PersonificacionDTO personificacion)
+        {
+            await _httpClient.PostAsJsonAsync("api/Personificaciones", personificacion);
+        }
+        public async Task UpdatePersonificaciones(int id, PersonificacionDTO personificacion)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Personificaciones/{id}", personificacion);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Error al actualizar las Personificaciones: {errorMessage}");
+            }
+        }
+        public async Task DeletePersonificaciones(int id)
+        {
+            await _httpClient.DeleteAsync($"api/Personificaciones/{id}");
+        }
+    }
 }
